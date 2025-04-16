adc4c3e5a80ea230beb06445203247343d898a82
Vaibhav Singhal <90049368+WebBhav@users.noreply.github.com>
2025-04-17 02:42:40 +0530

    Update InterviewRoom.jsx

--- src/InterviewRoom.jsx
+++ src/InterviewRoom.jsx
@@ -64,7 +64,7 @@ const InterviewRoom = () => {
   };
 
   const fetchGeminiResponse = async (userInput) => {
-    const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+    const API_KEY =VITE_GEMINI_API_KEY;
     const res = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
       {
